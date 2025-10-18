91f9e51e40dd17068a08afeec94dd3659877eb36
Antonio Capece <devantox01@gmail.com>
2025-10-19 00:06:01 +0200

    Add short name to commands

--- src/commands.c
+++ src/commands.c
@@ -3,20 +3,26 @@
 #include "utils.h"
 
 struct {
+    char *short_name;
     char *name;
     Command_Func *func;
 } commands[] = {
-    { .name = "search", .func = editor_search_word},
-    { .name = "open", .func = editor_open},
-    { .name = "write", .func = editor_write},
-    { .name = "bnext", .func = editor_buffers_next},
-    { .name = "bprev", .func = editor_buffers_prev},
-    { .name = "remove-selection", .func = editor_remove_selection},
+    { .short_name = "s", .name = "search", .func = editor_search_word},
+    { .short_name = "o", .name = "open", .func = editor_open},
+    { .short_name = "w", .name = "write", .func = editor_write},
+    { .short_name = "q", .name = "quit", .func = editor_quit},
+    { .short_name = "bn", .name = "bnext", .func = editor_buffers_next},
+    { .short_name = "bp", .name = "bprev", .func = editor_buffers_prev},
+    { .short_name = "rs", .name = "remove-selection", .func = editor_remove_selection},
 };
 
 Command_Func *command_search_name(String_View sv) {
     for(size_t i = 0; i < ARRAY_LEN(commands); i++) {
         char *cname = commands[i].name;
+        char *cshort = commands[i].short_name;
+        if(strlen(cshort) == sv.count && strncmp(cshort, sv.data, sv.count) == 0) {
+            return commands[i].func;
+        }
         if(strlen(cname) == sv.count && strncmp(cname, sv.data, sv.count) == 0) {
             return commands[i].func;
         }
