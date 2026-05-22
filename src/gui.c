3ccba6ddc89a8cffabc6cd536dd3e16855100755
Antonio Capece <devantox01@gmail.com>
2026-05-23 01:03:40 +0200

    Add render of the cursor and make window resizable

--- src/gui.c
+++ src/gui.c
@@ -24,6 +24,7 @@
 #define SCREEN_HEIGHT 600
 
 #define FONT_SIZE 48
+#define FONT_SCALE 0.5
 
 typedef struct {
     unsigned int texture;
@@ -46,6 +47,8 @@ typedef struct {
     unsigned int shader_program;
 } Cursor_Renderer;
 
+Cursor_Renderer cursor_renderer = {0};
+
 typedef struct {
     unsigned char r;
     unsigned char g;
@@ -57,6 +60,9 @@ typedef struct {
 #define BLUE  ((Color) {.r = 0x0,  .b = 0xff, .g = 0x0,  .a = 0xff})
 #define GREEN ((Color) {.r = 0x0,  .b = 0x0,  .g = 0xff, .a = 0xff})
 #define WHITE ((Color) {.r = 0xff, .b = 0xff, .g = 0xff, .a = 0xff})
+#define BLACK ((Color) {.r = 0x0, .b = 0x0, .g = 0x0, .a = 0x0})
+
+Editor ute = {.screen_height = SCREEN_HEIGHT, .screen_width = SCREEN_WIDTH};
 
 const char *vertex_shader_str = 
 "#version 330 core\n"
@@ -74,7 +80,7 @@ const char *vertex_shader_str =
 "    text_coords = position.zw;\n"
 "}";
 
-const char *fragment_shader_str =
+const char *text_fragment_shader_str =
 "#version 330 core\n"
 "\n"
 "in vec2 text_coords;\n"
@@ -86,6 +92,19 @@ const char *fragment_shader_str =
 "void main()\n"
 "{\n"
 "    frag_color = text_color * vec4(1.0, 1.0, 1.0, texture(text, text_coords).r);\n"
+"}";
+
+const char *cursor_fragment_shader_str =
+"#version 330 core\n"
+"\n"
+"out vec4 frag_color;\n"
+"\n"
+"uniform sampler2D text;\n"
+"uniform vec4 cursor_color;\n"
+"\n"
+"void main()\n"
+"{\n"
+"    frag_color = cursor_color;\n"
 "}";
 
 bool init_text_renderer() {
@@ -151,7 +170,20 @@ bool init_text_renderer() {
     return true;
 }
 
-bool compile_shaders() {
+bool init_cursor_renderer() {
+    glGenVertexArrays(1, &cursor_renderer.vao);
+    glGenBuffers(1, &cursor_renderer.vbo);
+    glBindVertexArray(cursor_renderer.vao);
+    glBindBuffer(GL_ARRAY_BUFFER, cursor_renderer.vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glBindVertexArray(0);
+
+    return true;
+}
+
+bool compile_text_shaders() {
     int success;
     char info_log[512];
 
@@ -166,7 +198,7 @@ bool compile_shaders() {
     }
 
     int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_str, NULL);
+    glShaderSource(fragment_shader, 1, &text_fragment_shader_str, NULL);
     glCompileShader(fragment_shader);
     glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
     if(!success) {
@@ -181,7 +213,47 @@ bool compile_shaders() {
     glLinkProgram(text_renderer.shader_program);
     glGetProgramiv(text_renderer.shader_program, GL_LINK_STATUS, &success);
     if(!success) {
+        glGetShaderInfoLog(text_renderer.shader_program, 512, NULL, info_log);
+        fprintf(stderr, "ERROR: could not link program shader:\n%s\n", info_log);
+        return false;
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    return true;
+}
+
+bool compile_cursor_shaders() {
+    int success;
+    char info_log[512];
+
+    int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_str, NULL);
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        fprintf(stderr, "ERROR: could not compile vertex shader:\n%s\n", info_log);
+        return false;
+    }
+
+    int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &cursor_fragment_shader_str, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if(!success) {
         glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        fprintf(stderr, "ERROR: could not compile fragment shader:\n%s\n", info_log);
+        return false;
+    }
+
+    cursor_renderer.shader_program = glCreateProgram();
+    glAttachShader(cursor_renderer.shader_program, vertex_shader);
+    glAttachShader(cursor_renderer.shader_program, fragment_shader);
+    glLinkProgram(cursor_renderer.shader_program);
+    glGetProgramiv(cursor_renderer.shader_program, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(cursor_renderer.shader_program, 512, NULL, info_log);
         fprintf(stderr, "ERROR: could not link program shader:\n%s\n", info_log);
         return false;
     }
@@ -204,7 +276,7 @@ float measure_text(char *str, size_t str_len, float scale) {
 float render_text(char *str, size_t str_len, float x, float y, float scale, Color color) {
     glUseProgram(text_renderer.shader_program);
     glUniform4f(glGetUniformLocation(text_renderer.shader_program, "text_color"), (float)color.r/255.0, (float)color.g/255.0, (float)color.b/255.0, (float)color.a/255.0);
-    glUniform2f(glGetUniformLocation(text_renderer.shader_program, "screen_sizes"), (float) SCREEN_WIDTH, (float) SCREEN_HEIGHT);
+    glUniform2f(glGetUniformLocation(text_renderer.shader_program, "screen_sizes"), (float) ute.screen_width, (float) ute.screen_height);
     glActiveTexture(GL_TEXTURE0);
     glBindVertexArray(text_renderer.vao);
 
@@ -214,7 +286,7 @@ float render_text(char *str, size_t str_len, float x, float y, float scale, Colo
         Character ch = text_renderer.characters[str[i]];
 
         float xpos = penx + ch.bearing_left * scale;
-        float ypos = SCREEN_HEIGHT - FONT_SIZE*scale - y - (ch.height - ch.bearing_top) * scale;
+        float ypos = ute.screen_height - FONT_SIZE*scale - y - (ch.height - ch.bearing_top) * scale;
 
         float w = ch.width * scale;
         float h = ch.height * scale;
@@ -257,7 +329,32 @@ float render_text(char *str, size_t str_len, float x, float y, float scale, Colo
     return penx;
 }
 
-float render_cursor(float x, float y, float scale, Color color) {
+void render_cursor(float x, float y, float w, float h, Color color) {
+    glUseProgram(cursor_renderer.shader_program);
+    glUniform4f(glGetUniformLocation(cursor_renderer.shader_program, "cursor_color"), (float)color.r/255.0, (float)color.g/255.0, (float)color.b/255.0, (float)color.a/255.0);
+    glUniform2f(glGetUniformLocation(cursor_renderer.shader_program, "screen_sizes"), (float) ute.screen_width, (float) ute.screen_height);
+    glBindVertexArray(cursor_renderer.vao);
+
+    float ypos = ute.screen_height - y - h;
+
+    float vertices[6][4] = {
+        {x,     ypos + h, 0.0f, 0.0f},
+        {x,     ypos,     0.0f, 0.0f},
+        {x + w, ypos,     0.0f, 0.0f},
+
+        {x,     ypos + h, 0.0f, 0.0f},
+        {x + w, ypos,     0.0f, 0.0f},
+        {x + w, ypos + h, 0.0f, 0.0f},
+    };
+    glBindBuffer(GL_ARRAY_BUFFER, cursor_renderer.vbo);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ZERO);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glBindVertexArray(0);
 }
 
 char *shift_args(int *argc, char ***argv) {
@@ -280,11 +377,13 @@ void render_display(Editor *ute) {
 
     size_t width = ute->screen_width;
     size_t height = ute->screen_height;
+    
+    // NOTE: This assumes monospace characters
+    float char_width = measure_text("A", 1, FONT_SCALE);
+    float char_height = FONT_SCALE*FONT_SIZE;
 
     int cur_x = 0;
     int cur_y = 0;
-    
-    float scale = 0.8;
 
     size_t y = 0;
     size_t buffer_y = 0;
@@ -296,21 +395,28 @@ void render_display(Editor *ute) {
         while(curr_char < line.end) {
             if(buffer->sb.data[curr_char] == '\n') break;
             ute_da_append(display, buffer->sb.data[curr_char]);
-            if(measure_text(display->data, display->count, scale) >= width) {
+            if(measure_text(display->data, display->count, FONT_SCALE) >= width) {
                 display->count--;
                 break;
             }
             curr_char++;
         }
-        render_text(display->data, display->count, 0, y, scale, WHITE);
-        y+=scale*FONT_SIZE;
+        render_text(display->data, display->count, x, y, FONT_SCALE, WHITE);
+        y+=char_height;
         buffer_y++;
     }
 
     cur_y = cy - buffer->sy;
     cur_x = cx - buffer->sx;
     if(cur_y >= height) cur_y = height - 1;
-    render_cursor(cur_x, cur_y, scale, WHITE);
+    render_cursor(cur_x*char_width, cur_y*char_height, char_width, char_height, WHITE);
+}
+
+void window_resizes(RGFW_window *win, i32 w, i32 h) {
+    (void) win;
+    printf("Callback called, w:%d, h:%d\n", w, h);
+    ute.screen_width = w;
+    ute.screen_height = h;
 }
 
 int main(int argc, char **argv) {
@@ -319,11 +425,12 @@ int main(int argc, char **argv) {
     hints->minor = 3;
     RGFW_setGlobalHints_OpenGL(hints);
 
-    RGFW_window *window = RGFW_createWindow("UTE", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, RGFW_windowNoResize | RGFW_windowCenter | RGFW_windowOpenGL);
+    RGFW_window *window = RGFW_createWindow("UTE", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, RGFW_windowCenter | RGFW_windowOpenGL);
     if(window == NULL) {
         fprintf(stderr, "ERROR: could not open RGFW window\n");
         return 1;
     }
+    RGFW_setWindowResizedCallback(window_resizes);
     RGFW_window_makeCurrentContext_OpenGL(window);
 
     glEnable(GL_BLEND);
@@ -331,10 +438,11 @@ int main(int argc, char **argv) {
 
 
     if(!init_text_renderer()) return 1;
+    if(!init_cursor_renderer()) return 1;
 
-    if(!compile_shaders()) return 1;
+    if(!compile_text_shaders()) return 1;
+    if(!compile_cursor_shaders()) return 1;
 
-    Editor ute = {.screen_height = SCREEN_HEIGHT, .screen_width = SCREEN_WIDTH};
     shift_args(&argc, &argv);
 
     if(argc > 0) {
@@ -365,6 +473,9 @@ int main(int argc, char **argv) {
     while(RGFW_window_shouldClose(window) == RGFW_FALSE) {
         RGFW_event event;
         if(RGFW_window_checkEvent(window, &event) == RGFW_TRUE) {
+            if(event.type == RGFW_keyPressed) {
+                manage_key(&ute, event.key.sym);
+            }
         }
 
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
